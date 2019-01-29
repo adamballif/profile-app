391b198d478d7fd466bbd0dace22f1e57b788a48
Adam Ballif <adamballif@Adams-15-MacBook-Pro.local>
2019-01-28 22:01:32 -0700

    built iphone interface

---
+++ profile-app/profile-app/View/LogoCorners.swift
@@ -0,0 +1,23 @@
+//
+//  LogoCorners.swift
+//  profile-app
+//
+//  Created by Adam Ballif on 1/28/19.
+//  Copyright © 2019 Adam Ballif. All rights reserved.
+//
+
+import UIKit
+//
+//class logoCorners: UIImageView {
+//
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//       layer.cornerRadius = self.frame.height / 2.0
+//        layer.masksToBounds = true
+//    }
+//
+//}
+
+//}
+
+
